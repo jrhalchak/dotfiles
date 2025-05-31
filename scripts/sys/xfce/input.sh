f3a12e732837ea8e89ca26bd6eda5e50b8f70d64
J Halchak <jonathan@halchak.email>
2025-05-31 02:52:25 -0400

    Fix script path after relocation

--- scripts/sys/xfce/input.sh
+++ scripts/sys/xfce/input.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # picom -b --config ~/.config/picom.conf
-xmodmap ~/dotfiles/.Xmodmap
+xmodmap ~/dotfiles/linux/.Xmodmap
 xinput set-prop "pointer:Logitech Wireless Mouse MX Master 3" "libinput Natural Scrolling Enabled" 1
