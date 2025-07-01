34e11efc2809c0a7a56e1379cf857e412fbac5ce
Jonathan Halchak <jonathan.halchak@annalect.com>
2025-06-30 22:27:57 -0400

    Wezterm config updates

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -28,14 +28,170 @@ cfg.term = "xterm-256color"
 -- Visuals / Scheme
 -- ========================
 cfg.color_scheme = 'tokyonight'
-cfg.window_background_opacity = 0.8
-cfg.macos_window_background_blur = 50
+cfg.window_background_opacity = 0.7
+cfg.macos_window_background_blur = 60
+
+cfg.inactive_pane_hsb = {
+  saturation = 0.5,
+  brightness = 0.4,
+}
 
 -- ========================
--- UI
+-- UI / Tab bar / right-bar
 -- ========================
 -- cfg.tab_bar_at_bottom = true
-cfg.hide_tab_bar_if_only_one_tab = true
+-- cfg.hide_tab_bar_if_only_one_tab = false
+
+cfg.use_fancy_tab_bar = false
+
+cfg.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#0b0022',
+
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = '#2b2042',
+      -- The color of the text for the tab
+      fg_color = '#c0c0c0',
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab_hover`.
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = '#1b1032',
+      fg_color = '#808080',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = '#3b3052',
+      fg_color = '#909090',
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab_hover`.
+    },
+  },
+}
+
+local function tabtitle(tab)
+  local title = tab.tab_title
+  if title and #title > 0 then
+    return title
+  end
+
+  return tab.active_pane.title
+end
+
+w.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local is_active = tab.is_active
+  local SOLID_LEFT_ARROW = w.nerdfonts.pl_right_hard_divider
+  local SOLID_RIGHT_ARROW = w.nerdfonts.pl_left_hard_divider
+
+  local edge_bg = "#0b0022"
+  local active_fg = "#2b2042"
+  local inactive_fg = "#1b1032"
+  local active_bg = edge_bg
+  local inactive_bg = edge_bg
+
+  local left_arrow = {
+    { Background = { Color = edge_bg } },
+    { Foreground = { Color = is_active and active_fg or inactive_fg } },
+    { Text = SOLID_LEFT_ARROW },
+  }
+
+  local title = {
+    { Background = { Color = is_active and active_fg or inactive_fg } },
+    { Foreground = { Color = "#c0c0c0" } },
+    { Text = " " .. tabtitle(tab) .. " " },
+  }
+
+  local right_arrow = {
+    { Background = { Color = edge_bg } },
+    { Foreground = { Color = is_active and active_fg or inactive_fg } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+
+  return w.format(left_arrow) .. w.format(title) .. w.format(right_arrow)
+end)
+
+w.on("update-right-status", function(window, pane)
+	local cwd = " "..pane:get_current_working_dir():sub(8).." "; -- remove file:// uri prefix
+	local date = w.strftime(" %I:%M %p  %A  %B %-d ");
+	local hostname = " "..w.hostname().." ";
+
+	window:set_right_status(
+		w.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#005f5f"}},
+			{Text=cwd},
+		})..
+		w.format({
+			{Foreground={Color="#00875f"}},
+			{Background={Color="#005f5f"}},
+			{Text=""},
+		})..
+		w.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#00875f"}},
+			{Text=date},
+		})..
+		w.format({
+			{Foreground={Color="#00af87"}},
+			{Background={Color="#00875f"}},
+			{Text=""},
+		})..
+		w.format({
+			{Foreground={Color="#ffffff"}},
+			{Background={Color="#00af87"}},
+			{Text=hostname},
+		})
+	);
+end);
 
 -- ========================
 -- Fonts
@@ -156,7 +312,6 @@ cfg.keys = {
     mods = "LEADER",
     action = act.SpawnTab("CurrentPaneDomain"),
   },
-
   {
     key = "p",
     action = act.ActivateTabRelative(-1),
